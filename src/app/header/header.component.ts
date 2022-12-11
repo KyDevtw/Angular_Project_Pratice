b57345517efc0ad1ee3450e83ef3af1548bad951
Kurt <k3030789@gmail.com>
2022-12-11 23:01:47 +0800

    add ngIf or routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -6,5 +6,11 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent {
+  // 讓 parent compo 可以讀到 featureSelected
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature)
+  }
 
 }
