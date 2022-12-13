8d73950ec360244d6b75d7bb12f22744dab4f0f2
Kurt <k3030789@gmail.com>
2022-12-13 23:52:12 +0800

    passing data

--- src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Recipes } from '../../recipes.model';
 
 @Component({
   selector: 'app-recipes-item',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-item.component.scss']
 })
 export class RecipesItemComponent {
+  @Input() recipe: Recipes;
+  @Output() recipeSelected = new EventEmitter<void>();
 
+  onSelected(){
+    this.recipeSelected.emit();
+  }
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Recipes } from './../recipes.model';
 
@@ -8,11 +8,16 @@ import { Recipes } from './../recipes.model';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent{
+  @Output() recipeWasSelected = new EventEmitter<Recipes>()
   recipes: Recipes[] = [
     new Recipes("A Test Recipe", "It's just test", "https://i.pinimg.com/564x/d1/a7/67/d1a767238ec823ea952981e388edc145.jpg"),
     new Recipes("A Test Recipe2", "It's just test2", "https://i.pinimg.com/564x/d1/a7/67/d1a767238ec823ea952981e388edc145.jpg")
   ];
 
   constructor(){}
+
+  onRecipeSelected(recipes: Recipes){
+    this.recipeWasSelected.emit(recipes)
+  }
 
 }
